b23e6696b77534f779744f9b56c713168dcda36d
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-01 21:57:52 +0000

    initial push for task 0

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+res = ["is zero", "is positive", "is negative"]
+if number == 0:
+	print(f"{number} {res[0]}")
+elif number > 0:
+	print(f"{number} {res[1]}")
+else:
+	print(f"{number} {res[2]}")
