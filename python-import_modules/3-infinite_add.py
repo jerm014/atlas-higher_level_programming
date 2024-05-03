a62074dcb48c01cea4717f95af94d341e75d43e2
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-03 22:29:42 +0000

    initial push for task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ != '__main__':
+    exit
+
+import sys
+res = 0
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        res += int(sys.argv[i])
+    print(res)
