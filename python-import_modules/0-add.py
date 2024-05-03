3a17ce9da063e250184e33f6ade175587d11733d
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-03 20:29:25 +0000

    moved if statement

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
+import add_0 as add
+a = 1
+b = 2
 if __name__ != '__main__':
-    import add_0 as add
-    a = 1
-    b = 2
     print("{0} + {1} = {2}".format(a, b, add.add(a, b)))
