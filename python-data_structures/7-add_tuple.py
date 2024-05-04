ee4e6c294ffab9b6299ed549a41024cc0f407623
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-04 20:06:56 +0000

    corrected all cases of => to >=

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -9,13 +9,13 @@ def add_tuple(a=(), b=()):
     res3 = 0
     res4 = 0
 
-    if len(a) => 1:
+    if len(a) >= 1:
         res1 = a[0]
-    if len(a) => 2:
+    if len(a) >= 2:
         res2 = a[1]
-    if len(b) => 1:
+    if len(b) >= 1:
         res3 = b[0]
-    if len(b) => 2:
+    if len(b) >= 2:
         res4 = b[1]
 
     return tuple((res1 + res2, res3 + res4))
