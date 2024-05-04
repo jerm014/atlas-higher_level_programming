da686b45dd2395da9c565d3aedd44b6a00c00352
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-04 19:55:11 +0000

    initial commit for tas 7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ != '__main__':
+    exit
+
+
+def add_tuple(a=(), b=()):
+    c = []
+
+    if len(a) == 0:
+        a = [0, 0]
+    elif len(a) == 1:
+        a[1] = 0
+
+    if len(b) == 0:
+        b = [0, 0]
+    elif len(b) == 1:
+        b[1] = 0
+
+    c[0] = a[0] + b[0]
+    c[1] = a[1] + b[1]
+    return c
