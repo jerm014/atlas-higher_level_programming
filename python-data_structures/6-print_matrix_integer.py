b40f26270b9bbe7976967f653de2e2646c093f33
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-04 19:05:27 +0000

    initial push for task 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ != '__main__':
+    exit
+
+
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end="")
+            if j != i[-1]:
+                print(" ", end="")
+        print()
