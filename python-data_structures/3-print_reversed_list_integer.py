d3855fdcd725c893269707c82947218818aa4511
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-04 18:41:54 +0000

    typo on line 9

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -6,5 +6,5 @@ if __name__ != '__main__':
 def print_reversed_list_integer(a=[]):
     i = len(a) - 1
     while i > 0:
-        print("{}".format(a[i]))
+        print("{:d}".format(a[i]))
         i -= 1
