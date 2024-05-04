1d5f4421cea63244fa9fa7ed89bf15ec00d6cc08
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-04 18:55:24 +0000

    initial push for task 5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ != '__main__':
+    exit
+
+def no_c(a):
+    res = ""
+    for i in range(0, len(a)):
+        if a[i] != "c" and a[i] != "C":
+            res =+ a[i]
+    return res
+
