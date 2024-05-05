108e092e9b5b2f9ac37132b8a8770bbae29d1e0a
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-05 20:04:54 +0000

    corrected check for set 2

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -14,4 +14,14 @@ def only_diff_elements(set_1, set_2):
         finally:
             if x == -1:
                 res.append(i)
+
+    for i in set_2:
+        try:
+            x = list(set_1).index(i)
+        except ValueError as e:
+            x = -1
+        finally:
+            if x == -1:
+                res.append(i)
+    
     return res
