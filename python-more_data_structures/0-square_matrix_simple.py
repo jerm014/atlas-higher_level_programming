6b2dc83b14c638487014ec48435b65fcdd8100bb
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-05 17:55:00 +0000

    pycode requires 2 lines before every def

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -6,8 +6,10 @@ if __name__ != '__main__':
 def square_matrix_simple(a=[]):
     return list(map(square_matrix, a))
 
+
 def square_matrix(a):
     return list(map(pow2, a))
+
 
 def pow2(a):
     return a ** 2
