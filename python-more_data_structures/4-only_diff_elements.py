c755d1e9a5ea49d1c872a3da738ef3864d42a2f8
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-05 20:03:14 +0000

    initial push for task 4

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ != '__main__':
+    exit
+
+
+def only_diff_elements(set_1, set_2):
+    res = []
+    x = -1
+    for i in set_1:
+        try:
+            x = list(set_2).index(i)
+        except ValueError as e:
+            x = -1
+        finally:
+            if x == -1:
+                res.append(i)
+    return res
