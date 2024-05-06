10b645bf327c2b0ce9e2bd3ced1d165f74bba9fa
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-06 22:24:11 +0000

    done for the day

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,6 +3,19 @@ if __name__ != '__main__':
     exit
 
 
-def safe_print_list_integers(my_list=[], x=0):
+def safe_print_list_integers(a=[], b=0):
+    printed = 0
     try:
-        
+        for i in range(0, b + 1):
+            print(a[i], end="")
+            printed += 1
+            if printed == b:
+                return printed
+    except (TypeError, ValueError):
+        pass
+    except (IndexError) as e:
+        raise IndexError(e)
+    finally:
+        print()
+
+    return printed
