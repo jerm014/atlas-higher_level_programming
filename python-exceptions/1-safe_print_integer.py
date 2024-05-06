29a803bd894440e40ca239b50fcee6b45b311be7
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-06 21:50:19 +0000

    initial commit for 1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,3 +4,8 @@ if __name__ != '__main__':
 
 
 def safe_print_integer(value):
+    try:
+        print()"{:d}".format(value))
+        return True
+    except Exception as e:
+        return False
--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,4 +4,5 @@ if __name__ != '__main__':
 
 
 def safe_print_list_integers(my_list=[], x=0):
-
+    try:
+        
