9885f69d2b83b2b21f42d196ceb2498eba70b9b8
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-06 21:04:55 +0000

    initial file creation for project

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,3 +3,16 @@ if __name__ != '__main__':
     exit
 
 
+def safe_print_list(a=[], x=0):
+    res = 0
+    try:
+        for i in a:
+            print(i, end="")
+            res += 1
+            if res == x:
+                break
+    except:
+        return res
+    finally:
+        print()
+        return res
