c842b06d51cd5a396c6d3ff5d17256d8b7fa89be
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-06 17:10:19 +0000

    initial push for taks 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,12 +1,3 @@
 #!/usr/bin/python3
-if __name__ != '__main__':
-    exit
-
-
-def multiply_list_map(a=[], number=0):
-    iter2 = [number] * len(a)
-    return list(map(mul, a, iter2))
-
-
-def mul(a, b):
-    return a * b 
+def multiply_list_map(a=[], b=0):
+    return list(map(lambda x: x * b, a))
