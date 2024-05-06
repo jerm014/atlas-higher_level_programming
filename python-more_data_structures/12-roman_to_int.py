c6bea95676eab879901a5e971272ac95f3859c8d
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-06 17:28:36 +0000

    initial push for roman task

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -4,12 +4,15 @@ if __name__ != '__main__':
 
 
 def roman_to_int(roman_string):
-    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    sub = {"IV" : 4, "IX" : 9, "XL": 40, "XC": 90, "CD": 400, "CM" : 900}
-    others1 = {"III": 3, "XXX": 30, "CCC": 300, "MMM": 3000}
-    others2 = {"II": 2, "XX" : 20, "CC": 200, "MM": 2000}
-    
-    for i in range(0,len(roman_string)-2):
-        # find out if there are any cases of subtraction?
-        if roman_string[i: i + 1] == "IV"
+    roman = {"III": 3, "XXX": 30, "CCC": 300, "MMM": 3000, "II": 2, "XX" : 20, \
+            "CC": 200, "MM": 2000, "IV" : 4, "IX" : 9, "XL": 40, "XC": 90, \
+            "CD": 400, "CM" : 900, "I": 1, "V": 5, "X": 10, "L": 50, "C": 100, \
+            "D": 500, "M": 1000}
+    res = 0
 
+    if roman_string != "" and isinstance(roman_string, str):
+        for i in roman:
+            if roman_string.find(i) != -1:
+                roman_string = roman_string.replace(i, "")
+                res += roman[i]
+    return res
