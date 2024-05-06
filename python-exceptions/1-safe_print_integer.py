441824bef1cee9804aace2c8446b51dd3132b10f
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-06 21:51:42 +0000

    1

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -5,7 +5,7 @@ if __name__ != '__main__':
 
 def safe_print_integer(value):
     try:
-        print()"{:d}".format(value))
+        print("{:d}".format(value))
         return True
     except Exception as e:
         return False
