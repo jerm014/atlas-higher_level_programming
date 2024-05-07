bf7b13ee569d200167ceeb2964a520b89b30324a
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-07 05:36:05 +0000

    crrected line 10

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ if __name__ != '__main__':
 def safe_print_list_integers(a=[], b=0):
     printed = 1
     try:
-        for i in range(0, b + 1):
+        for i in range(0, b):
             if printed == b:
                 return printed
             print("{:d}".format(a[i]), end="")
