633229a4b6bc4fef2774fb951ccd528ba002f243
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-07 06:19:05 +0000

    initial attempt for task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,5 +2,21 @@
 if __name__ != '__main__':
     exit
 
-    
-def list_division(my_list_1, my_list_2, list_length):
+
+def list_division(a, b, c):
+    errors = {"ZeroDivisionError": "division by 0",
+              "TypeError": "wrong type",
+              "IndexError": "out of range"}
+    res = []
+    for i in range(c):
+        try:
+            div = a[i] / b[i]
+        except IndexError:
+            print(errors["IndexError"])
+        except TypeError:
+            print(errors["TypeError"])
+        except ZeroDivisionError:
+            print(errors["ZeroDivisionError"])
+        finally:
+            res.append(div)
+    return res
