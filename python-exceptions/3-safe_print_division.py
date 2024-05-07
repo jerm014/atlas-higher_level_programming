52c31bb0719be9618417ff659c1cb9701c68cb9a
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-07 06:10:06 +0000

    first attempt for task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -4,3 +4,13 @@ if __name__ != '__main__':
 
     
 def safe_print_division(a, b):
+    res = None
+    try:
+        res = a / b
+    except ZeroDivisionError:
+        pass
+    except Exception as e:
+        print("Exception:{}".format(e))
+    finally:
+        print("Inside result: {}".format(res))
+        return res
