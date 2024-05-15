241b8eb627e914c2d0cfafb1c7b7be95570519de
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-15 00:08:44 +0000

    handle only first name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -21,4 +21,7 @@ def say_my_name(first_name, last_name=""):
     elif not isinstance(last_name, str):
         raise TypeError(err_last)
     else:
-        print("My name is {} {}".format(first_name, last_name))
+        if not last_name == "":
+            print("My name is {} {}".format(first_name, last_name))
+        else:
+            print("My name is {}".format(first_name))
