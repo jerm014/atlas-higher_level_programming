b0ce62b1c9b1c047d1230eeac6743c8fcfbe074a
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 03:42:49 +0000

    first try for 4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Check if object is instance inherited from the specified class"""
+
+
+def inherits_from(obj, some_class):
+    """Return True if object is an instance of a class that inherited from the
+    specified class
+    """
+    return (issubclass(type(obj), a_class) and (some_class is not type(obj)))
