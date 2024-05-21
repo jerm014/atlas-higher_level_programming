656e89a93ba08ab54fedb52099668c3819e36929
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 17:48:20 +0000

    error message correction

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -4,12 +4,12 @@
 
 def read_file(filename=""):
     """documentation for the function!"""
-    ERR_NOFILE = "[FileNotFoundError] [Errno 2] No such file or directory: '{}'"
+    ERR_NOFILE = "[FileNotFoundError] [Errno 2] No such file or directory: "
     try:
         with open(filename, "r", encoding="utf-8") as file:
             for line in file:
                 print(line, end="")
     except FileNotFoundError:
-        print(ERR_NOFILE, filename)
+        print(ERR_NOFILE, "'", filename, "'")
     except Exception as e:
         print(f"An error occurred: {e}")
