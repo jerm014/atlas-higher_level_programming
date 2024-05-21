2e0e1f86b39c8699cbdfa124b322d8f734a07289
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 03:44:15 +0000

    indentation?

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,4 +6,5 @@ def inherits_from(obj, some_class):
     """Return True if object is an instance of a class that inherited from the
     specified class
     """
-    return (issubclass(type(obj), some_class) and (some_class is not type(obj)))
+    return (issubclass(type(obj), some_class) and
+        (some_class is not type(obj)))
