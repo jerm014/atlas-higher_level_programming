fa0791715fbb6d799ac9abd50918dd797ed41462
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 03:48:33 +0000

    first push for 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""documentation"""
+
+
+class BaseGeometry:
+    """more documentaiton"""
+
+   def area(self):
+        """nothing to see here"""
+        e = "area() is not implemented"
+        raise Exception(e)
