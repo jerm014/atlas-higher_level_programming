0ab860eb9cf1291bf4151f807dc52efbb455e2d9
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 18:37:36 +0000

    push for 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""documentation is important!"""
+import json
+
+def load_from_json_file(filename):
+    """
+    Loads a JSON file and returns the corresponding Python object.
+    
+    Args:
+        filename: The name of the input JSON file.
+    
+    Returns:
+        The Python object parsed from the JSON file.
+    """
+    try:
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        print(f"Error decoding JSON: {e}")
+        return None
