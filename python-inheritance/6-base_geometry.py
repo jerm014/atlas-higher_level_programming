55c0726b7725c3a63c87c554a45532d22bdf5fcd
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 03:49:11 +0000

    missing a dumb space. python UGH

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,7 +5,7 @@
 class BaseGeometry:
     """more documentaiton"""
 
-   def area(self):
+    def area(self):
         """nothing to see here"""
         e = "area() is not implemented"
         raise Exception(e)
