96b80a92d6c7e2245677cfe0215e8800183eeb0d
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-21 18:56:01 +0000

    can't import? don't know.

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,14 +5,45 @@ import sys
 import json
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file.py').load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file using a JSON representation.
 
+    Args:
+        my_obj (object): The Python object to be serialized.
+        filename (str):  The name of the file to write.
+
+    Notes:
+        - Uses the 'with' statement for proper file handling.
+        - Does not handle exceptions if the object cannot be serialized.
+        - Does not manage file permission exceptions.
+    """
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
+
+
+def load_from_json_file(filename):
+    """
+    Loads a JSON file and returns the corresponding Python object.
+    
+    Args:
+        filename: The name of the input JSON file.
+    
+    Returns:
+        The Python object parsed from the JSON file.
+    """
     try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        with open(filename, 'r') as file:
+            return json.load(file)
+    except json.JSONDecodeError as e:
+        print(f"[JSONDecodeError] {e}")
+    except FileNotFoundError as e:
+        print(f"[FileNotFoundError] {e}")
+
+
+try:
+    items = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, "add_item.json")
