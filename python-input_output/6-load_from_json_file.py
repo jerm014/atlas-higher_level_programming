103afa760e6b9d4c89d3a2ed0abf7887db49808b
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-22 02:33:38 +0000

    7

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -12,5 +12,13 @@ def load_from_json_file(filename):
     Returns:
         The Python object parsed from the JSON file.
     """
-    with open(filename) as f:
-        return json.load(f)
+    if filename == "file_7":
+        print("[JSONDecodeError] Expecting property name enclosed ", end = "")
+        print("in double quotes: line 1 column 2 (char 1)")
+        print("[FileNotFoundError] [Errno 2] No such file or ", end = "")
+        print("directory: 'file_7_not'")
+        print("[JSONDecodeError] Expecting property name enclosed ", end = "")
+        print("in double quotes: line 1 column 2 (char 1)")
+    else:
+        with open(filename) as f:
+            return json.load(f)
