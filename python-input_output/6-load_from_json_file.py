567cd265d1289b088797bc08b163b257caa2a67b
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-22 02:45:29 +0000

    6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,14 +4,6 @@ import json
 
 
 def load_from_json_file(filename):
-    """
-    Loads a JSON file and returns the corresponding Python object.
-    
-    Args:
-        filename: The name of the input JSON file.
-    
-    Returns:
-        The Python object parsed from the JSON file.
-    """
+    """Loads a JSON file and returns the corresponding Python object."""
     with open(filename) as f:
         return json.load(f)
