8645d101dce29bb49c0a76dfadc8c4e0780d420e
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-22 03:13:06 +0000

    triangles

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""LATE"""
+
+
+def pascal_triangle(i):
+    """Dumb Triangle"""
+    if i <= 0:
+        return []
+
+    pt = [[1]]
+    while len(pt) != i:
+        triangle = pt[-1]
+        tmp = [1]
+        for x in range(len(triangle) - 1):
+            temp.append(triangle[x] + triangle[x + 1])
+        temp.append(1)
+        pt.append(temp)
+    return pt
