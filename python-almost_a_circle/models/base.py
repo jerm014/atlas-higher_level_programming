e306be5c6e05392ee199d3bfa1008992037f1aea
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-27 23:21:01 +0000

    = is assignment. == is comparison. will I ever learn?

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -47,7 +47,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """create a new thing from dictionary information"""
-        if cls.__name__ = "Rectangle":
+        if cls.__name__ == "Rectangle":
             obj = cls(1, 1)
         else:
             obj = cls(1)
