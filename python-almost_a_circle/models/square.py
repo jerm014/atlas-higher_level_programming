ebb08c1207337bc216e00b17ab08ca20d18a2663
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-27 18:06:30 +0000

    first square

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""File for class Rectangle, which inherits from class Base"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """class Square, inherited from Rectangle"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """init a square"""
+        super().__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """print out details of this square"""
+        s = "[Square] (" + str(self.id) + ") "
+        s += str(self.__x) + " / " + str(self.__y) + " - "
+        s += str(self.__width)
+        return s
