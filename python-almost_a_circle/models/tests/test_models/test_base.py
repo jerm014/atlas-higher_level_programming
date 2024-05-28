db9b2c2d0329696b56af3243b34c164837be431e
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-28 11:27:53 +0000

    test cases

---
+++ python-almost_a_circle/models/tests/test_models/test_base.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Defines unittests for base.py.
+Unittest classes:
+    TestBase_instantiation - line 14
+
+"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestBase_instantiation(unittest.TestCase):
+    """test instantiation of the class Base"""
+    def test_make_a_base(self):
+        baseA = Base()
+        baseB = Base()
+        self.assertEquals(baseA.id, baseB.id - 1)
+
---
+++ python-almost_a_circle/models/tests/test_models/test_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines unittests for base.py.
+Unittest classes:
+
+"""
+import os
+import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
