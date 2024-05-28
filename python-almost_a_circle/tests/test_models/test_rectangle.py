1335e9f3a52956c474308c4019026b18d0f7c324
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-28 12:20:11 +0000

    test 2

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Defines unittests for base.py.
-Unittest classes:
+"""unittests for rectangle.py"""
 
-"""
 import os
 import unittest
 from models.base import Base
@@ -11,9 +9,13 @@ from models.square import Square
 
 
 class Test_Rectangle(unittest.TestCase):
-    """test instantiation of the class Base"""
+    """test instantiation of the class Rectangle"""
 
     def test_new_rectangle(self):
+        """can we make rectangles"""
         r1 = Rectangle(1, 2)
         r2 = Rectangle(1, 2)
         self.assertEqual(r1.id, r2.id - 1)
+
+    def test_new_rectangle2(self):
+        self.assertIsNotNone(Rectangle(1, 2))
