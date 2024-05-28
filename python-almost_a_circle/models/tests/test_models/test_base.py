d68c15cba14913389194f1c9d467053391c64c17
jerm014 <jeremy.mitts@atlasschool.com>
2024-05-28 11:45:01 +0000

    documentation

--- python-almost_a_circle/models/tests/test_models/test_base.py
+++ python-almost_a_circle/models/tests/test_models/test_base.py
@@ -14,17 +14,21 @@ from models.square import Square
 class TestBase_instantiation(unittest.TestCase):
     """test instantiation of the class Base"""
     def test_new_base(self):
+        """can we make a new Base"""
         base = Base()
         self.assertEqual(base.id, 1)
 
     def test_make_a_base(self):
+        """do the id's make sense"""
         baseA = Base()
         baseB = Base()
         self.assertEquals(baseA.id, baseB.id - 1)
 
     def test_assign_id(self):
+        """can we assign an id ourselves"""
         self.assertEqual(98, Base(98).id)
 
     def test_makes_a_base(self):
+        """another test of making a new Base with default id"""
         self.assertIsNotNone(Base())
 
