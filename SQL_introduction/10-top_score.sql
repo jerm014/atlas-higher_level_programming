b49e54f5bb15f5b8eb5ca6c50a94a4ccf1f1ff82
jerm014 <jeremy.mitts@atlasschool.com>
2024-06-13 22:47:25 +0000

    documentation

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- "The proper use of comments is to compensate for our failure to express
+-- ourself in code." --Robert C. Martin (Author of Clean Code)
+--
+-- Make sure you put in a comment so everyone knows that this simple command
+--  lists all records of the table sorted by score desc
+SELECT score, name FROM second_table ORDER BY score DESC;
--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -2,7 +2,8 @@
 -- ourself in code." --Robert C. Martin (Author of Clean Code)
 --
 -- Make sure you put in a comment so everyone knows that this simple command
--- shows the count of records in a table in a database specified on the command line
+-- creates a table in a database specified on the command line
+-- adds four records to the table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) 
 VALUES (1, 'John', 10),
