7f6632d8d2f161e38d60408f03a4377bfb09a610
jerm014 <jeremy.mitts@atlasschool.com>
2024-06-13 22:54:31 +0000

    documentation

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+ -- "The proper use of comments is to compensate for our failure to express
+-- ourself in code." --Robert C. Martin (Author of Clean Code)
+--
+-- Make sure you put in a comment so everyone knows that this simple command
+-- computes the score average of all records in the table
+SELECT score, COUNT(score) AS number from second_table ORDER BY number DESC;
