6486d92a92488eeb94294e69c4798a74581830b9
jerm014 <jeremy.mitts@atlasschool.com>
2024-06-13 22:10:01 +0000

    task 6

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,7 @@
+-- "The proper use of comments is to compensate for our failure to express
+-- ourself in code." --Robert C. Martin (Author of Clean Code)
+--
+-- Make sure you put in a comment so everyone knows that this simple command
+-- shows the definition of a table in a database specified on the command line
+USE ?;
+SELECT * FROM first_table;
