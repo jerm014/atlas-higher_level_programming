f2be26e2eee0aa554f21064c66029b606c829be8
jerm014 <jeremy.mitts@atlasschool.com>
2024-06-13 22:50:18 +0000

    documentation

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,6 @@
+-- "The proper use of comments is to compensate for our failure to express
+-- ourself in code." --Robert C. Martin (Author of Clean Code)
+--
+-- Make sure you put in a comment so everyone knows that this simple command
+-- updates the score of Bob to 10 in the table second_table
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
