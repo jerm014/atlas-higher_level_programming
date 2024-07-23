5a97646508d9fffe8428f4c7fbd6d54eda480e70
Jeremy Mitts <jermitts@gmail.com>
2024-07-23 17:58:32 -0500

    javascript. boring. task 12 object

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// This one is weird. Am I missing something?
+const myObject = {
+    type: 'object',
+    value: 12
+};
+
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
