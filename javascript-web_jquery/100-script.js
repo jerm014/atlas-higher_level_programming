955f052f9c45e1ee1007aa3d672a527dfa3c6a6c
Jeremy Mitts <jermitts@gmail.com>
2024-07-30 11:21:48 -0500

    cleanup

--- javascript-web_jquery/100-script.js
+++ javascript-web_jquery/100-script.js
@@ -1,6 +1,5 @@
 function makeRed() {
-  if (document.readyState === 'complete')
-  {
+  if (document.readyState === 'complete') {
     const style = document.createElement('style');
     style.innerHTML = 'header { color: red; }';
     document.head.appendChild(style);
