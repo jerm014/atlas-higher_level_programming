4f83e9bdfb3d1ccefa5803f100192c626e75bf4f
jerm014 <jeremy.mitts@atlasschool.com>
2024-07-29 23:00:40 -0500

    execute bit set

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+let count = 0;
+
+if (url === undefined) process.exit(1);
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body).results;
+    data.filter(df => df.characters.forEach(element => {
+      if (element.indexOf('18') > 0) count++;
+    }))
+  };
+  console.log(count);
+});
