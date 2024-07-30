be09dbc921444da57570d6ed1c9a8a16208371b5
jerm014 <jeremy.mitts@atlasschool.com>
2024-07-29 23:09:21 -0500

    execute bit set

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+const request = require('request');
+const output = {};
+
+if (url === undefined) process.exit(1);
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    results = JSON.parse(body).filter((resfil) => resfil.completed);
+    results.forEach(node => {
+      if (output[node.userId]) {
+        output[node.userId] += 1;
+      } else { 
+        output[node.userId] = 1;
+      }
+    });
+    console.log(output);
+  }
+});
