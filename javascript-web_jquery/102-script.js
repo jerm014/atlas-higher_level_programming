1c530156c58025531729eb612f2098086dfd73c0
Jeremy Mitts <jermitts@gmail.com>
2024-07-30 13:43:48 -0500

    done with bonus 13

--- javascript-web_jquery/102-script.js
+++ javascript-web_jquery/102-script.js
@@ -1,8 +1,8 @@
 const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
 const langs = 'ar, az, be, bg, bn, bs, cs, da, de, dz, el, en, en-gb, ' +
   'en-us, es, et, fa, fi, fil, fr, he, hi, hr, hu, hy, id, is, it, ja, ' +
-  'ka, kk, km, ko, lb, lo, lt, lv, mk, mn, ms, my, ne, no, pl, pt, ro, ru, ' +
-  'sk, sl, sq, sr, sv, sw, th, tk, uk, vi, zh';
+  'ka, kk, km, ko, lb, lo, lt, lv, mk, mn, ms, my, ne, no, pl, pt, ro, ' +
+  'ru, sk, sl, sq, sr, sv, sw, th, tk, uk, vi, zh';
 
 function getLang() {
   let lang = $('INPUT#language_code').val();
